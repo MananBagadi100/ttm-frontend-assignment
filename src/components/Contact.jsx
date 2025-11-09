855f26b94a71d00fc653b8430101659115e97a7a
MananBagadi100 <mananbagadi100@gmail.com>
2025-11-09 12:32:19 +0530

    I added react hook form to stimulate how my frontend would send any request to the backend

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,48 @@
 import "../Styles/ContactStyles.css";
+import {useForm} from "react-hook-form";
 
 export default function Contact() {
-  return (
-    <section className="contact">
-      <div className="contact-content">
-        <h2>Get in Touch</h2>
-        <p>
-          Have questions or want a demo? We’re here to help you streamline your fleet operations.
-        </p>
+    const {register, handleSubmit, reset} = useForm();
 
-        <form className="contact-form">
-          <input type="text" placeholder="Your Name" required />
-          <input type="email" placeholder="Email Address" required />
-          <textarea placeholder="Message" rows="4" required></textarea>
-          <button type="submit">Send Message</button>
-        </form>
-      </div>
-    </section>
-  );
+    const onSubmit = (data) => {
+        console.log("Form Submitted:", data);
+
+        // Simulate "submission success"
+        alert("Message submitted ✅ (check console)");
+
+        reset();
+    };
+
+    return (
+        <section className="contact">
+            <div className="contact-content">
+                <h2>Get in Touch</h2>
+                <p>
+                    Have questions or want a demo? We’re here to help you streamline your fleet
+                    operations.
+                </p>
+
+                <form className="contact-form" onSubmit={handleSubmit(onSubmit)}>
+                    <input
+                        {...register("name", { required: true })}
+                        type="text"
+                        placeholder="Your Name"/>
+
+                    <input
+                        {...register("email", { required: true })}
+                        type="email"
+                        placeholder="Email Address"/>
+
+                    <textarea
+                        {...register("message", { required: true })}
+                        placeholder="Message"
+                        rows="4"></textarea>
+
+                    <button type="submit">
+                        Send Message
+                    </button>
+                </form>
+            </div>
+        </section>
+    );
 }
